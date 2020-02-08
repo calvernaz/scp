1cc40599373d783d38dfaa0fbdb05b4880d923d0
Cesar Alvernaz <calvernaz@weirdloop.org>
2020-02-08 19:39:07 +0000

    add wip

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -19,7 +19,7 @@ func TestMatchStatement(t *testing.T) {
 	}
 
 	if len(program.Statements) != 1 {
-		t.Fatalf("program.Statements does not contain 1 statment. got=%d", len(program.Statements))
+		t.Fatalf("program.Statements does not contain 1 statement. got=%d", len(program.Statements))
 	}
 
 	tests := []struct {
