fa6f2fcfb65bf031fabb4fefe8d37ba44a98cb3f
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2020-06-24 07:59:33 +0100

    add test for include statement

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -12,63 +12,6 @@ import (
 	"github.com/calvernaz/scp/token"
 )
 
-//func TestMatchStatement(t *testing.T) {
-//	input := `Match host "some-domain.com"`
-//
-//	l := lexer.New(input)
-//	p := New(l)
-//
-//	program := p.ParseConfig()
-//	if program == nil {
-//		t.Fatalf("ParseConfig() return nil")
-//	}
-//
-//	if len(program.Statements) != 1 {
-//		t.Fatalf("program.Statements does not contain 1 statement. got=%d", len(program.Statements))
-//	}
-//
-//	tests := []struct {
-//		expectedString string
-//	}{
-//		{"Match"},
-//	}
-//
-//	for i, tt := range tests {
-//		stmt := program.Statements[i]
-//		if !testMatchConfigStatement(t, stmt, tt.expectedString) {
-//			return
-//		}
-//	}
-//}
-
-//func testMatchConfigStatement(t *testing.T, s ast.Statement, name string) bool {
-//	if s.TokenLiteral() != "Match" {
-//		t.Errorf("s.TokenLiteral not 'Match'. got=%q", s.TokenLiteral())
-//	}
-//
-//	configStmt, ok := s.(*ast.Match)
-//	if !ok {
-//		t.Errorf("s not *ast.ConfigStatement. got=%T", s)
-//		return false
-//	}
-//
-//	if configStmt.Token.Literal != name {
-//		t.Errorf("configStmt.Name not '%s'. got=%s", name, configStmt.Token)
-//		return false
-//	}
-//
-//	if configStmt.TokenLiteral() != name {
-//		t.Errorf("configStmt.TokenLiteral() not '%s'. got=%s", name, configStmt.TokenLiteral())
-//		return false
-//	}
-//
-//	if configStmt.Condition != "host" {
-//		t.Errorf("configStmt.Condition not '%s'. got=%s", "host", configStmt.Condition)
-//		return false
-//	}
-//
-//	return true
-//}
 
 func TestSshConfig(t *testing.T) {
 	file, err := os.Open("testdata/ssh_config")
@@ -121,6 +64,27 @@ func TestSshConfig2(t *testing.T) {
 				}
 			}
 		}
+	}
+}
+
+func TestSshIncludeConfig(t *testing.T) {
+	file, err := os.Open("testdata/include_config")
+	if err != nil {
+		t.FailNow()
+	}
+
+	input, err := ioutil.ReadAll(file)
+	if err != nil {
+		t.FailNow()
+	}
+
+	l := lexer.New(string(input))
+	p := New(l)
+	program := p.ParseConfig()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 8 {
+		t.Fatalf("program does not contain %d statements. got=%d\n", 8, len(program.Statements))
 	}
 }
 
